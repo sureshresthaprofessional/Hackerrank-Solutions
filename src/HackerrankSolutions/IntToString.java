8736718f1b88d9bfba373511c27d5659ad562496
Suresh Shrestha <sureshresthaprofessional@gmail.com>
2025-06-08 11:50:35 +1000

    Adding Question

--- src/HackerrankSolutions/IntToString.java
+++ src/HackerrankSolutions/IntToString.java
@@ -1,5 +1,18 @@
 package HackerrankSolutions;
 
+/*
+ * Question
+ * You are given an integer , you have to convert 
+ * it into a string.
+ * Please complete the partially completed code in the editor. 
+ * If your code successfully converts  into a string  the code 
+ * will print "Good job". Otherwise it will print "Wrong answer".
+ * can range between  to  inclusive.
+ * 
+ * */
+
+
+
 import java.util.*;
 import java.security.*;
 public class IntToString {
